a03c776b1fe2b823e08605ace0744bebda5bb629
TrKhanq <trongkhangnguyen153@gmail.com>
2026-05-13 12:54:39 +0700

    Refactor hospital database SQL script
    Renamed database and updated SQL commands for consistency. Added new queries for updating and selecting from the view.

--- exx1.sql
+++ exx1.sql
@@ -1,25 +1,44 @@
-CREATE DATABASE hospital_db;
-
-USE hospital_db;
+CREATE DATABASE HospitalDB;
+USE HospitalDB;
 
 CREATE TABLE Patients (
-	Patient_ID INT PRIMARY KEY,
-	Full_Name VARCHAR(100),
-	Age INT,
-	Room_Number INT,
-	HIV_Status VARCHAR(50),
-	Mental_Health_History VARCHAR(255)
+    Patient_ID INT PRIMARY KEY,
+    Full_Name VARCHAR(100),
+    Age INT,
+    Room_Number INT,
+    HIV_Status VARCHAR(50),
+    Mental_Health_History VARCHAR(255)
 );
--- Chèn một số dữ liệu
-INSERT INTO Patients (Patient_ID, Full_Name, Age, Room_Number, HIV_Status, Mental_Health_History)
+
+INSERT INTO Patients 
+(Patient_ID, Full_Name, Age, Room_Number, HIV_Status, Mental_Health_History)
 VALUES
-(1, 'Minh Thu', 30, 101, 'Negative', 'None' ),
-(2, 'Hồng Vân', 40, 102, 'Positive', 'Anxiety'),
-(3, 'Cao Cường', 25, 103, 'Negative', 'None');
+(1, 'Minh Thu', 30, 101, 'Negative', 'None'),
+(2, 'Hong Van', 40, 102, 'Positive', 'Anxiety'),
+(3, 'Cao Cuong', 25, 103, 'Negative', 'None');
 
 CREATE VIEW Reception_Patient_View AS
-SELECT Patient_ID, Full_Name, Age, Room_Number FROM Patients WHERE Age >= 0 WITH CHECK OPTION ;
+SELECT 
+    Patient_ID,
+    Full_Name,
+    Age,
+    Room_Number
+FROM Patients
+WHERE Age >= 0
+WITH CHECK OPTION;
 
-INSERT INTO Reception_Patient_View (Patient_ID, Full_Name, Age, Room_Number)
-VALUES
-(5, 'Thien Bao', -10, 101);
+SELECT * FROM Reception_Patient_View;
+
+UPDATE Reception_Patient_View
+SET Age = 35
+WHERE Patient_ID = 1;
+
+SELECT * FROM Reception_Patient_View;
+
+UPDATE Reception_Patient_View
+SET Age = -5
+WHERE Patient_ID = 2;
+
+SELECT * FROM Patients;
+
+DROP VIEW Reception_Patient_View;
